4d0024497acad6efcaeedf486f3268bba9704475
tekt <tekt@immerse.studio>
2025-07-14 10:39:31 -0700

    state transfer and general structuring

--- index.mjs
+++ index.mjs
@@ -1,7 +1,104 @@
 import {discover} from 'loupedeck';
 import * as osc from 'osc';
 
-const portNumber = 9239;
+const inputPortNumber = 9240;
+const outputPortNumber = 9239;
+
+class Connector {
+	constructor(device, oscOut, oscIn) {
+		this.device = device;
+		this.oscOut = oscOut;
+		this.oscIn = oscIn;
+
+		this.curOpPath = null;
+		this.curParName = null;
+		this.curParInfo = null;
+	}
+
+	open() {
+		this.oscIn.on('error', this.onOscInError.bind(this));
+		this.oscIn.on('message', this.onOscMessage.bind(this));
+
+		this.oscOut.on('error', this.onOscOutError.bind(this));
+
+		this.device.on('connect', this.onDeviceConnect.bind(this));
+		this.device.on('down', this.onButtonDown.bind(this));
+		this.device.on('rotate', this.onEncoderRotate.bind(this));
+
+		this.oscIn.open();
+		this.oscOut.open();
+	}
+
+	setCurrentTarget(opPath, parName, parInfo) {
+		this.curOpPath = opPath;
+		this.curParName = parName;
+		this.curParInfo = parInfo;
+		console.log(`Current target set to ${opPath}:${parName}`);
+		this.updateDisplay();
+	}
+
+	onDeviceConnect() {
+		console.log('Device connected');
+	}
+
+	onButtonDown(button) {
+		console.log('Button down:', button);
+	}
+
+	onEncoderRotate(encoder) {
+		console.log('Encoder rotate:', encoder);
+	}
+
+	onOscInError(error) {
+		console.error('OSC in error:', error);
+	}
+
+	onOscOutError(error) {
+		console.error('OSC out error:', error);
+	}
+
+	onOscMessage(message, timeTag, info) {
+		switch (message.address) {
+			case '/loupedeck/target/setPar':
+				if (message.args.length === 3) {
+					const opPath = message.args[0].value;
+					const parName = message.args[1].value;
+					const parInfoStr = message.args[2].value;
+					const parInfo = JSON.parse(parInfoStr);
+					this.setCurrentTarget(opPath, parName, parInfo);
+				} else {
+					console.error('Invalid arguments for /loupedeck/setCurrentTarget');
+				}
+				break;
+			case '/loupedeck/target/clear':
+				this.setCurrentTarget(null, null, null);
+				break;
+			default:
+				console.warn('Unrecognized OSC message received:', message);
+		}
+	}
+
+	updateDisplay() {
+		this.device.drawScreen('center', (c, w, h) => {
+			c.fillStyle = '#990033';
+			c.fillRect(0, 0, w, h);
+			if (this.curOpPath) {
+				c.font = '20px Arial';
+				c.fillStyle = 'white';
+				c.fillText(this.curOpPath, 10, 30);
+			}
+			if (this.curParInfo) {
+				c.font = '16px Arial';
+				c.fillStyle = 'white';
+				let text = this.curParInfo.label;
+				if (this.curParInfo.subLabel) {
+					text += ` (${this.curParInfo.subLabel})`;
+				}
+				c.fillText(text, 10, 60);
+			}
+		});
+	}
+}
 
 async function main() {
 	let device;
@@ -17,33 +114,30 @@ async function main() {
 
 	const oscOut = new osc.default.UDPPort({
 		remoteAddress: 'localhost',
-		remotePort: portNumber,
+		remotePort: outputPortNumber,
 	});
-	oscOut.on('error', (error) => {
-		console.error('OSC error:', error);
+	const oscIn = new osc.default.UDPPort({
+		localAddress: '0.0.0.0',
+		localPort: inputPortNumber,
+		metadata: true,
 	});
-	oscOut.open();
 
-	device.on('connect', () => {
-		console.log('Device connected');
-	});
-	device.on('down', (button) => {
-		console.log('Button down:', button);
-	});
-	device.on('rotate', (encoder) => {
-		console.log('Encoder rotate:', encoder);
-		device.drawKey(0, (c) => {
-			c.fillStyle = 'red';
-			c.fillRect(0, 0, 100, 100);
-			c.fillStyle = 'white';
-			c.font = '20px Arial';
-			c.fillText('change ' + encoder.toString(), 10, 30);
-		});
-		oscOut.send({
-			address: '/loupedeck/encoder',
-			args: [encoder.toString()],
-		});
-	});
+	const connector = new Connector(device, oscOut, oscIn);
+	connector.open();
+	// device.on('rotate', (encoder) => {
+	// 	console.log('Encoder rotate:', encoder);
+	// 	device.drawKey(0, (c) => {
+	// 		c.fillStyle = 'red';
+	// 		c.fillRect(0, 0, 100, 100);
+	// 		c.fillStyle = 'white';
+	// 		c.font = '20px Arial';
+	// 		c.fillText('change ' + encoder.toString(), 10, 30);
+	// 	});
+	// 	oscOut.send({
+	// 		address: '/loupedeck/encoder',
+	// 		args: [encoder.toString()],
+	// 	});
+	// });
 }
 
 main().catch(console.error);
--- td/loupedeckConnector.py
+++ td/loupedeckConnector.py
@@ -1,3 +1,5 @@
+import json
+
 class LoupedeckConnector:
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
@@ -7,4 +9,28 @@ class LoupedeckConnector:
 		print('Receive OSC', address, repr(args))
 	
 	def onCurParChange(self, par):
-		pass
+		if par is None or par.isOP or par.isString or par.isSequence:
+			self.oscOut.sendOSC('/loupedeck/target/clear', [''])
+		else:
+			self.oscOut.sendOSC('/loupedeck/target/setPar', [par.owner.path, par.name, json.dumps(_parInfo(par))])
+
+def _parInfo(par):
+	return {
+		'name': par.name,
+		'label': par.label,
+		'subLabel': par.subLabel if par.subLabel != par.label else None,
+		'style': par.style,
+		'mode': par.mode.name,
+		'enable': par.enable,
+		'vecIndex': par.vecIndex,
+		'readOnly': par.readOnly,
+		'min': par.min,
+		'max': par.max,
+		'clampMin': par.clampMin,
+		'clampMax': par.clampMax,
+		'default': par.default,
+		'normMin': par.normMin,
+		'normMax': par.normMax,
+		'menuNames': par.menuNames,
+		'menuLabels': par.menuLabels,
+	}
